ec18e4a2c138228aa29444af12591f89d02e04c9
hadar <hadarush_pur@hotmail.com>
2017-10-17 13:53:46 +0300

    add the search activity

--- app/src/main/java/com/example/hadar/parkit/FindParkingSpaceActivity.java
+++ app/src/main/java/com/example/hadar/parkit/FindParkingSpaceActivity.java
@@ -1,13 +1,47 @@
 package com.example.hadar.parkit;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class FindParkingSpaceActivity extends AppCompatActivity {
-
+    private Button search;
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_parking_space);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        findViews();
+        addListenersToButtons();
+    }
+
+    void findViews() {
+        search = (Button) findViewById(R.id.search);
+        spinner = (Spinner)  findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.walk_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void addListenersToButtons() {
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FindParkingSpaceActivity.this, Top5ParkingSpaceActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hadar/parkit/Top5ParkingSpaceActivity.java
@@ -0,0 +1,13 @@
+package com.example.hadar.parkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Top5ParkingSpaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top5_parking_space);
+    }
+}
