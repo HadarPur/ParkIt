1af887bcf8e60c3bd377a8bf4343d7c7fae4cda4
hadar <hadarush_pur@hotmail.com>
2017-10-30 13:04:38 +0200

    add animation

--- app/src/main/java/com/example/hadar/parkit/UI/SplashActivity.java
+++ app/src/main/java/com/example/hadar/parkit/UI/SplashActivity.java
@@ -4,15 +4,35 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 import com.example.hadar.parkit.R;
 
 public class SplashActivity extends AppCompatActivity {
     public static int SPLASH_OUT=1000;
+    private ImageView loading;
+    public RotateAnimation rotate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loading = (ImageView)findViewById(R.id.imageView);
+        rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF,
+                0.5f,  Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(SPLASH_OUT);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loading.startAnimation(rotate);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -22,5 +42,4 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, SPLASH_OUT);
     }
-
 }
