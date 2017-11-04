363154bd762c794bb4947857f7851fe5caa4d855
hadar <hadarush_pur@hotmail.com>
2017-11-04 22:05:15 +0200

    close the keyboard on search

--- app/src/main/java/com/example/hadar/parkit/UI/StatisticsActivity.java
+++ app/src/main/java/com/example/hadar/parkit/UI/StatisticsActivity.java
@@ -1,5 +1,6 @@
 package com.example.hadar.parkit.UI;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -119,6 +121,7 @@ public class StatisticsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 streetName = txt.getText().toString();
+                hideKeyboard(StatisticsActivity.this);
                 String txt = spinner1.getSelectedItem().toString();
                 position = spinner1.getSelectedItemPosition();
                 Log.d(TAG," "+ txt + " position : "+position);
@@ -191,6 +194,18 @@ public class StatisticsActivity extends AppCompatActivity {
             return false;
         }
     }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
     //start loading view fot the callback
     public void loadingPage() {
         loadingBack.setVisibility(View.VISIBLE);
